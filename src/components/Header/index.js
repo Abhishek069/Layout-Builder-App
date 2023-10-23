6cd27afce6843e7511aef96bb83306fb46fd7016
Abhishek069 <abhishinde0723@gmail.com>
2023-10-23 05:09:17 +0000

    commit message

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+// import LanguageContext from '../../context/LanguageContext'
+
+import './index.css'
+
+const Header = () => (
+  <div className="Home-card-container">
+    <div className="card-container">
+      <h1 className="font-header">Header</h1>
+    </div>
+  </div>
+)
+
+export default Header
